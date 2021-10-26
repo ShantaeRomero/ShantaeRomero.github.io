3d387e801e305e568e0ae451b57aebb8eb68c4c7
ShantaeRomero <shantae.romero.student@madisonpsb.org>
2021-10-26 15:21:52 +0000

    add collectable to game

--- projects/platformer/js/init/collectable.js
+++ projects/platformer/js/init/collectable.js
@@ -38,6 +38,9 @@
         
         // example: 
         createCollectable(type.steve, 200, 170, 6, 0.7);
+        createCollectable(type.grace, 100, 300);
+        createCollectable(type.db, 400, 120);
+        createCollectable(type.max, 500, 200);
         
         
         
